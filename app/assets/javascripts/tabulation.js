b22e8e563a889adadcff330a8d381af501a542eb
BeAnjara <beanjarahoussen@gmail.com>
2019-11-21 05:30:46 +0300

    tab pills ready

--- app/assets/javascripts/tabulation.js
+++ app/assets/javascripts/tabulation.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
 	let tabulation = $(".tab");
-	
 	tabulation.hide();
 	tabulation.eq(0).show();
 
-});
+	$('ul.tab-links li a').click(function(e){
+		e.preventDefault();
+		let a = $(this).attr('href');
+		tabulation.hide();
+		$('div.tab-content div'+a).show();
+	});
 
+});
