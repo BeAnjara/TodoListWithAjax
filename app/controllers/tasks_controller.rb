5eb6e37c68255d2f3dc582dca9b4b4021c927f91
BeAnjara <beanjarahoussen@gmail.com>
2019-11-22 15:42:29 +0300

    ajax

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,10 +9,16 @@ before_action :authenticate_user!
     @category = Category.find(category_params)
     @task.category = @category
     if @task.save
-      redirect_to root_path
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js { }
+      end
       flash[:notice] = "Task created"
     else
-      redirect_to root_path
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js { }
+      end
       flash[:notice] = "Please try again"
     end
   end
